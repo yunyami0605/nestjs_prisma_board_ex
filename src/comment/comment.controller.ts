b02dd486595c6298f3b4a9f1fb745ba5eafd4224
yunyami <yunyami0605@gmail.com>
2023-04-02 17:59:06 +0900

    [Feat] comment swagger 추가
    1. comment swagger 추가

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -11,12 +11,19 @@ import {
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiPostDecorator } from 'src/customDecorator/swagger/post.decorator';
 
+@ApiTags('COMMENT API')
 @Controller('comment')
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
   @Post(':postId')
+  @ApiPostDecorator({
+    summary: '댓글 생성 api',
+    DtoType: CreateCommentDto,
+  })
   create(
     @Body() createCommentDto: CreateCommentDto,
     @Param('postId') postId: string,
@@ -27,16 +34,25 @@ export class CommentController {
   }
 
   @Get('all')
+  @ApiOperation({
+    summary: '댓글 전체 조회 api',
+  })
   findAll() {
     return this.commentService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: '댓글 조회 api',
+  })
   findOne(@Param('id') id: string) {
     return this.commentService.findOne(+id);
   }
 
   @Get('/cursor/:postId')
+  @ApiOperation({
+    summary: '댓글 리스트 커서 api',
+  })
   findCommentCursor(
     @Param('postId') postId: string,
     @Query('cursorId') cursorId?: string,
@@ -45,21 +61,33 @@ export class CommentController {
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: '댓글 수정 api',
+  })
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentService.update(+id, updateCommentDto);
   }
 
+  @ApiOperation({
+    summary: '댓글 삭제 api',
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.commentService.remove(+id);
   }
 
   @Post('like/:id')
+  @ApiOperation({
+    summary: '댓글 좋아요 api',
+  })
   like(@Param('id') id: string) {
     return this.commentService.like(+id);
   }
 
   @Post('dislike/:id')
+  @ApiOperation({
+    summary: '댓글 싫어요 api',
+  })
   dislike(@Param('id') id: string) {
     return this.commentService.dislike(+id);
   }
--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateCommentDto {
+  @ApiProperty({
+    required: true,
+    example: 'test_content',
+  })
   @IsString()
   content: string;
 }
