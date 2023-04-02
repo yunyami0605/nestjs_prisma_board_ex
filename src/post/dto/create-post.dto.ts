b6e0e1f845f4844e66c4892bd704e00e7c50e6d4
yunyami <yunyami0605@gmail.com>
2023-04-02 17:54:50 +0900

    [Feat] post swagger 추가
    1. post swagger 추가

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,24 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({
+    required: true,
+    example: 'test_title',
+  })
   @IsString()
   readonly title: string;
 
+  @ApiProperty({
+    required: true,
+    example: 'test_content',
+  })
   @IsString()
   readonly content: string;
 
+  @ApiProperty({
+    example: '2',
+  })
   @IsOptional()
   @IsNumber()
   readonly authorId?: number;
 
+  @ApiProperty({
+    type: [String],
+    example: ['태그1', '태그2'],
+  })
   @IsNotEmpty({ each: true })
   @IsString({ each: true })
-  // @ApiProperty({
-  //   name: 'tags',
-  //   type: [String],
-  //   description: 'tags',
-  //   example: ['햄스터', '피부병'],
-  //   required: true,
-  // })
   tags: string[];
 }
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -15,13 +15,27 @@ import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { AccessGuard } from 'src/auth/guard/AccessGuard';
 import { RequestWithUser } from 'src/auth/interface/requestWithUser.interface';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { PostDecorator } from 'src/customDecorator/swagger/post.decorator';
 
+@ApiTags('POST API')
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
   @UseGuards(AccessGuard)
   @Post()
+  @PostDecorator({
+    summary: '유저 생성',
+    DtoType: CreatePostDto,
+  })
   create(@Body() createPostDto: CreatePostDto, @Req() req: RequestWithUser) {
     return this.postService.create({
       ...createPostDto,
@@ -30,41 +44,68 @@ export class PostController {
   }
 
   @Get()
+  @ApiOperation({
+    summary: '게시글 리스트 페이지네이션 형식 조회 api',
+  })
   findPage(@Query('page') page: string) {
     return this.postService.findPage(+page);
   }
 
   @Post('like/:id')
+  @ApiOperation({
+    summary: '게시글 좋아요 api',
+  })
   like(@Param('id') id: string) {
     return this.postService.like(+id);
   }
 
   @Post('dislike/:id')
+  @ApiOperation({
+    summary: '게시글 싫어요 api',
+  })
   dislike(@Param('id') id: string) {
     return this.postService.dislike(+id);
   }
 
   @Get('cursor')
+  @ApiOperation({
+    summary: '게시글 리스트 커서 방식 조회 api',
+  })
+  @ApiParam({
+    name: '',
+  })
   findPageCursor(@Query('search') search?: string, @Query('id') id?: string) {
     return this.postService.findPageCursor(search, +id);
   }
 
   @Get('all')
+  @ApiOperation({
+    summary: '전체 게시글 조회 api',
+  })
   findAll() {
     return this.postService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: ' 게시글 조회 api',
+  })
   findOne(@Param('id') id: string) {
     return this.postService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: '게시글 수정 api',
+  })
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postService.update(+id, updatePostDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: '게시글 삭제 api',
+  })
   remove(@Param('id') id: string) {
     return this.postService.remove(+id);
   }
