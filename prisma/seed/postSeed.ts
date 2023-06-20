5d9e60beda6e95714ce92c6ac0a192f4e1e3a145
yunyami <yunyami0605@gmail.com>
2023-06-20 16:54:40 +0900

    feat: code-push test

--- prisma/seed/postSeed.ts
+++ prisma/seed/postSeed.ts
@@ -4,11 +4,11 @@ import _ from 'lodash';
 const prisma = new PrismaClient();
 
 export async function postSeed() {
-  const dataLen = new Array(20).fill(0).map((item, i) => i + 1000);
+  const dataLen = new Array(14).fill(0).map((item, i) => i + 1000);
 
   const _data = dataLen.map((_item, i) => ({
-    title: `test_title_${_item}`,
-    content: `test_content_${_item}`,
+    title: `happy_title_${_item}`,
+    content: `calern_content_${_item}`,
     authorId: 1,
   }));
 
