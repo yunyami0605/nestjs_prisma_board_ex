42d21770a6ff4233c6a3dff016d8c2263e6792e4
yunyami <yunyami0605@gmail.com>
2023-06-21 16:01:46 +0900

    refactor: after-deploy shell script 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -3,20 +3,32 @@ REPOSITORY=/home/ec2-user/nestjs_prisma_board_ex
 YARN_PATH=~/.nvm/versions/node/v16.18.1/bin/yarn
 PM2_PATH=~/.nvm/versions/node/v16.18.1/bin/pm2
 
+
 cd $REPOSITORY
 
-sudo env "PATH=$PATH" $YARN_PATH
+which yarn
+which pm2
 
-sudo env "PATH=$PATH" $YARN_PATH build
 
-sudo env "PATH=$PATH" $PM2_PATH restart
 
-sudo env "PATH=$PATH" $PM2_PATH start serverapp
+$YARN_PATH
 
+$YARN_PATH build
+
+$PM2_PATH restart
+
+$PM2_PATH start serverapp
+
+# sudo env "PATH=$PATH" $YARN_PATH
+
+# sudo env "PATH=$PATH" $YARN_PATH build
+
+# sudo env "PATH=$PATH" $PM2_PATH restart
+
+# sudo env "PATH=$PATH" $PM2_PATH start serverapp
 
 # mkdir test2
 
-# which yarn
 
 # sudo ln -s /root/.nvm/versions/node/v16.18.1/bin/yarn /usr/bin/yarn
 
